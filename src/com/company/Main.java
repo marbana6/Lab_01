b08c167a638cff3e670087a96d6af6884ab1d2ef
Marta <martabanaszek82@gmail.com>
2021-04-08 19:35:29 +0200

    Main test nowy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("TEST");
 	    System.out.println("Mój drugi commit");
+	    System.out.println("Nowy test");
+	    System.out.println("Main test");
+
 
         // write your code here
     }
