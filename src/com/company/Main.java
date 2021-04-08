21879083741916fdc0ba8777c25763918ca54d6c
Marta <martabanaszek82@gmail.com>
2021-04-08 10:14:23 +0200

    My second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("TEST");
+	    System.out.println("Mój drugi commit");
+
         // write your code here
     }
 
